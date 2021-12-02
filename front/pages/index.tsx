ed768a320cbf9c908841b09e66b01974b892e5cd
Keravi <cmkrosp@gmail.com>
2021-12-03 05:24:32 +0900

    [ADD] Scroll Top Button

--- front/pages/index.tsx
+++ front/pages/index.tsx
@@ -5,10 +5,42 @@ import Skills from "../src/components/Skills";
 import Archiving from "../src/components/Archiving";
 import Projects from "../src/components/Projects";
 import Greetings from "../src/components/Greetings";
+import { useEffect, useState } from "react";
+import { UpSquareOutlined } from "@ant-design/icons";
+import styled from "styled-components";
+
+const ScrollTopWrapper = styled.a`
+  :hover {
+    color: inherit;
+  }
+`;
 
 const Home: NextPage = () => {
+  const [scrollTop, setScrollTop] = useState("none");
+  useEffect(() => {
+    const onScroll = () => {
+      if (window.scrollY > 500) {
+        setScrollTop("inline");
+      } else {
+        setScrollTop("none");
+      }
+    };
+    window.addEventListener("scroll", onScroll);
+    return () => window.removeEventListener("scroll", onScroll);
+  }, []);
   return (
     <Container fluid>
+      <ScrollTopWrapper href="#">
+        <UpSquareOutlined
+          style={{
+            fontSize: "3rem",
+            position: "fixed",
+            bottom: "30px",
+            right: "30px",
+            display: `${scrollTop}`,
+          }}
+        />
+      </ScrollTopWrapper>
       <Row id="about">
         <Col lg={12} className="col-xl-8" xl={{ offset: 2 }}>
           <AboutMe />
